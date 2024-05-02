e27a25b34ad007bb2a8a5980d0033d661ee4dafd
Gullit Damião Teixeira de Campos <gullitdtdc@gmail.com>
2024-05-02 10:52:26 -0300

    Create Sorting_Function_Heap_Shell.h

---
+++ Sorting_Function_Heap_Shell/Sorting_Function_Heap_Shell.h
@@ -0,0 +1,16 @@
+#ifndef HEAP_H_INCLUDED
+#define HEAP_H_INCLUDED
+
+int esquerda(int i);
+
+int direita(int i);
+
+void shellSort(int vetor[], int n);
+
+void refazHeapMax(int V[], int i, int compHeap);
+
+void constroiHeapMax(int V[], int tamanho);
+
+void heapSort(int V[], int tamanho);
+
+#endif // HEAP_H_INCLUDED
