cfdec944899604c56818a9bc0513a339af40c612
Gullit Damião Teixeira de Campos <gullitdtdc@gmail.com>
2024-05-03 22:14:12 -0300

    Update Sorting_Function_Bubble_Selection_Insertion_Shell.h

---
+++ Time_BubbleSort_SelectionSort_InsertionSort_ShellSort/Sorting_Function_Bubble_Selection_Insertion_Shell.h
@@ -0,0 +1,16 @@
+#ifndef FUNCORD_H_INCLUDED
+#define FUNCORD_H_INCLUDED
+
+// Função para ordenar um vetor usando Bubble Sort
+void bubbleSort(int arr[], int n);
+
+// Função para ordenar um vetor usando SelectionSort
+void selectionSort(int vetor[], int n);
+
+// Função para ordenar um vetor usando InsertionSort
+void insertionSort(int vet[], int n);
+
+// Função para ordenar um vetor usando ShellSort
+void Shellsort(int vetor[], int n);
+
+#endif // FUNCORD_H_INCLUDED
