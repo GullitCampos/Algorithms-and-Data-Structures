1197848b473606f2586546a9693ce2e8a02f4353
Gullit Damião Teixeira de Campos <gullitdtdc@gmail.com>
2024-05-05 15:58:34 -0300

    Create Variations_QuickSort.h

---
+++ Variations_of_Quick Sort/Variations_QuickSort.h
@@ -0,0 +1,7 @@
+void troca(int v[], int a, int b);
+int* inverteVetor(int v[], int n);
+int particao(int vetor[], int esq, int dir);
+void quickSort(int v[], int esq, int dir);
+void quicksort_aleatorizado (int *v, int esq, int dir);
+void quicksort_mediana_tres (int v[], int esq, int dir);
+void quickSort_tres_partes(int a[], int l, int r);
